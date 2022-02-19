a279bc344df243b37b1db59c8213b8df23b220a3
t.gritsenko <t.gritsenko@g.nsu.ru>
2022-02-19 16:48:32 +0700

    Added non-adjacent frequency ranges support

--- Reverb/MainWindow.cs
+++ Reverb/MainWindow.cs
@@ -23,46 +23,121 @@ namespace Reverb
     {
         private readonly WasapiCapture soundIn;
         private readonly WasapiOut soundOut;
-        private readonly DmoWavesReverbEffect reverbBass, reverbMiddle, reverbTreble;
+        private readonly DmoWavesReverbEffect reverbBass = null, reverbMiddle = null, reverbTreble = null;
+        private const int SampleRate = 44100;
+        private const int GeneralBF = 65, GeneralTF = 7500;
+        private const int ReverbBassBF = 65, ReverbBassTF = 300;
+        private const int ReverbMiddleBF = 700, ReverbMiddleTF = 1100;
+        private const int ReverbTrebleBF = 2000, ReverbTrebleTF = 3000;
         public MainWindow()
         {
             InitializeComponent();
+
+            //Initialize WasapiCapture for recording
             soundIn = new WasapiCapture(true, AudioClientShareMode.Shared, 0);
             soundIn.Initialize();
-            var source1 = new SoundInSource(soundIn) { FillWithZeros = true };
-            var source2 = new SoundInSource(soundIn) { FillWithZeros = true };
-            var source3 = new SoundInSource(soundIn) { FillWithZeros = true };
 
-            //Apply bandpass filter to each frequency group
-            var bassFilter = source1.ToSampleSource().AppendSource(x => new BiQuadFilterSource(x));
-            bassFilter.Filter = new HighpassFilter(bassFilter.WaveFormat.SampleRate, 100);
-            var bassSource = bassFilter.AppendSource(x => new BiQuadFilterSource(x));
-            bassSource.Filter = new LowpassFilter(bassSource.WaveFormat.SampleRate, 1200);
-
-            var middleFilter = source2.ToSampleSource().AppendSource(x => new BiQuadFilterSource(x));
-            middleFilter.Filter = new HighpassFilter(middleFilter.WaveFormat.SampleRate, 1200);
-            var middleSource = middleFilter.AppendSource(x => new BiQuadFilterSource(x));
-            middleSource.Filter = new LowpassFilter(middleSource.WaveFormat.SampleRate, 2300);
-
-            var trebleFilter = source3.ToSampleSource().AppendSource(x => new BiQuadFilterSource(x));
-            trebleFilter.Filter = new HighpassFilter(trebleFilter.WaveFormat.SampleRate, 2300);
-            var trebleSource = trebleFilter.AppendSource(x => new BiQuadFilterSource(x));
-            trebleSource.Filter = new LowpassFilter(trebleSource.WaveFormat.SampleRate, 3400);
-
-            //Apply reverb effect
-            reverbMiddle = new DmoWavesReverbEffect(middleSource.ToWaveSource());
-            reverbBass = new DmoWavesReverbEffect(bassSource.ToWaveSource());
-            reverbTreble = new DmoWavesReverbEffect(trebleSource.ToWaveSource());
-
-            //Initialize mixer
-            var mixer = new SimpleMixer(2, 44100)
+            //Initialize the mixer
+            var mixer = new SimpleMixer(2, SampleRate)
             {
                 FillWithZeros = true,
                 DivideResult = true
             };
-            mixer.AddSource(reverbBass.ToSampleSource().ChangeSampleRate(44100).ToStereo());
-            mixer.AddSource(reverbMiddle.ToSampleSource().ChangeSampleRate(44100).ToStereo());
-            mixer.AddSource(reverbTreble.ToSampleSource().ChangeSampleRate(44100).ToStereo());
+
+            //Split the input signal into different frequency ranges, apply reverb and add to the mixer
+            if (GeneralBF != ReverbBassBF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, GeneralBF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, ReverbBassBF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+
+            if (ReverbBassBF != ReverbBassTF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbBassBF);
+                var reverbBassSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                reverbBassSource.Filter = new LowpassFilter(SampleRate, ReverbBassTF);
+
+                reverbBass = new DmoWavesReverbEffect(reverbBassSource.ToWaveSource());
+                mixer.AddSource(reverbBass.ToSampleSource());
+            }
+
+            if (ReverbBassTF != ReverbMiddleBF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbBassTF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, ReverbMiddleBF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+
+            if (ReverbMiddleBF != ReverbMiddleTF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbMiddleBF);
+                var reverbMiddleSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                reverbMiddleSource.Filter = new LowpassFilter(SampleRate, ReverbMiddleTF);
+
+                reverbMiddle = new DmoWavesReverbEffect(reverbMiddleSource.ToWaveSource());
+                mixer.AddSource(reverbMiddle.ToSampleSource());
+            }
+
+            if (ReverbMiddleTF != ReverbTrebleBF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbMiddleTF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, ReverbTrebleBF);
+
+                mixer.AddSource(nonEffectSource);
+            }
+
+            if (ReverbTrebleBF != ReverbTrebleTF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbTrebleBF);
+                var reverbTrebleSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                reverbTrebleSource.Filter = new LowpassFilter(SampleRate, ReverbTrebleTF);
+
+                reverbTreble = new DmoWavesReverbEffect(reverbTrebleSource.ToWaveSource());
+                mixer.AddSource(reverbTreble.ToSampleSource());
+            }
+
+            if (ReverbTrebleTF != GeneralTF)
+            {
+                var source = new SoundInSource(soundIn) { FillWithZeros = true }
+                    .ChangeSampleRate(SampleRate).ToStereo().ToSampleSource();
+
+                var tempFilter = source.AppendSource(x => new BiQuadFilterSource(x));
+                tempFilter.Filter = new HighpassFilter(SampleRate, ReverbTrebleTF);
+                var nonEffectSource = tempFilter.AppendSource(x => new BiQuadFilterSource(x));
+                nonEffectSource.Filter = new LowpassFilter(SampleRate, GeneralTF);
+
+                mixer.AddSource(nonEffectSource);
+            }
 
             //Initialize the soundout with the mixer 
             soundOut = new WasapiOut() { Latency = 1 };
@@ -71,17 +146,20 @@ namespace Reverb
 
         private void Bass_Scroll(object sender, EventArgs e)
         {
-            reverbBass.ReverbTime = Bass.Value;
+            if (reverbBass != null)
+                reverbBass.ReverbTime = Bass.Value;
         }
 
         private void Middle_Scroll(object sender, EventArgs e)
         {
-            reverbMiddle.ReverbTime = Middle.Value;
+            if (reverbMiddle != null)
+                reverbMiddle.ReverbTime = Middle.Value;
         }
 
         private void Treble_Scroll(object sender, EventArgs e)
         {
-            reverbTreble.ReverbTime = Treble.Value;
+            if (reverbTreble != null)
+                reverbTreble.ReverbTime = Treble.Value;
         }
 
         private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
